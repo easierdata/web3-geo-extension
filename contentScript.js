61935cac35236c1c8ff9a9bfd315e5b60d995318
matthewnanas <spazeplayz@gmail.com>
2024-01-11 12:28:01 -0500

    Better IPFS metadata fetching

--- contentScript.js
+++ contentScript.js
@@ -37,31 +37,40 @@ async function handleDownloadClick() {
     Get ipfs metadata if popup is displayed
 */
 async function getIPFSMetadata() {
-    await delay(500)
-    if (!popup) {
-        popup = true;
+    let cid = undefined;
+
+    // Wait for a CID to be present
+    while (cid === undefined) {
         const label = document.getElementsByClassName("ipfs-cid-text")[0]
-        const cid = label.innerText.split(" ")[2]
-        console.log(cid)
-
-        if (cid) {
-            const result = await chrome.runtime.sendMessage({ type: "get" });
-            
-            const response = await fetch(`http://${JSON.parse(result).node_ip}:${JSON.parse(result).node_port}/api/v0/dht/findprovs?arg=${cid}`, {
-                method: 'POST',
-            });
-    
-            const res = await response.text();
-            const peers = res.split('"Type":4').length - 1
-
-            if (response.status === 200) {
-                const pins = document.getElementsByClassName("pins")[0]
-                pins.innerText = `Pinned on ${peers} IPFS nodes`
-            }
+        if (label) {
+            cid = label.innerText.split(" ")[2]
+            console.log(cid)
+            break;
         }
-    } else {
-        const label = document.getElementsByClassName("ipfs-cid-text")[0]
-        if (!label) popup = false;
+        await delay(500)
+    }
+
+    // Fetch details with node
+    if (cid) {
+        const pins = document.getElementsByClassName("pins")[0]
+        pins.innerText = `Fetching ipfs node data...`
+        
+        const result = await chrome.runtime.sendMessage({ type: "get" });
+        
+        const response = await fetch(`http://${JSON.parse(result).node_ip}:${JSON.parse(result).node_port}/api/v0/dht/findprovs?arg=${cid}`, {
+            method: 'POST',
+        });
+
+        const res = await response.text();
+        const peers = res.split('"Type":4').length - 1
+
+        if (response.status === 200) {
+            const pins = document.getElementsByClassName("pins")[0]
+            pins.innerText = `Pinned on ${peers} IPFS nodes`
+            return;
+        } else {
+            alert("Failed to get IPFS metadata!")
+        }
     }
 }
 
@@ -70,12 +79,13 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 /* Handle clicks */
 document.addEventListener('click', function (event) {
     /* Add event listener to pinButton ID */
+    console.log(event.target);
     if (event.target.matches('#pinButton')) {
         handleButtonClick();
     }
     else if (event.target.matches('#downloadButton')) {
         handleDownloadClick();
-    } else {
+    } else if (event.target.matches('.mapboxgl-canvas')){
         getIPFSMetadata();
     }
 });
